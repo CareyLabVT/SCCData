c5c8b7598e1945fa5d99974f3b36b32d2eba5153
rqthomas <rqthomas>
2018-12-07 09:25:27 -0500

    fixed bug in timezone, now in GMT

--- grab-weekly-forecast-for-glm-v3.R
+++ grab-weekly-forecast-for-glm-v3.R
@@ -5,35 +5,29 @@ library(rNOMADS)
 library(RCurl)
 
 
-#Define location of interest
-
-time <- c(0,64) #6 hour prediction for 14 days
+#User defined location of interest and directory
 
 lake_lat_n = 37.27
-
 lake_lon_w = 79.9
-
-lon.dom <- seq(0, 360, by = 1) #domain of longitudes in model
-
-lat.dom <- seq(-90, 90, by = 1) #domain of latitudes in model
-
-lon <- which.min(abs(lon.dom  - (360 - lake_lon_w))) - 1 #NOMADS indexes start at 0
-
-lat <- which.min(abs(lat.dom - lake_lat_n)) - 1 #NOMADS indexes start at 0 
 
 directory = '/home/scc/data/SCCData/NOAA/'
 
-#Get yesterdays 6 am GMT, 12 pm GMT, 6 pm GMT, and todays 12 an GMT runs
+#####
 
+time <- c(0,64) #6 hour prediction for 16 days
+lon.dom <- seq(0, 360, by = 1) #domain of longitudes in model
+lat.dom <- seq(-90, 90, by = 1) #domain of latitudes in model
+lon <- which.min(abs(lon.dom  - (360 - lake_lon_w))) - 1 #NOMADS indexes start at 0
+lat <- which.min(abs(lat.dom - lake_lat_n)) - 1 #NOMADS indexes start at 0 
+
+#Get yesterdays 6 am GMT, 12 pm GMT, 6 pm GMT, and todays 12 an GMT runs
 urls.out <- GetDODSDates(abbrev = "gens")
 
 for(i in 1:length(urls.out$url)){
   
   model.url <- urls.out$url[i]
-  
   run_date <- urls.out$date[i]
-  
-  model_list <- c('gep_all_00z','gep_all_06z','gep_all_12z','gep_all_18z')
+  model_list <- c('gep_all_00z', 'gep_all_06z', 'gep_all_12z', 'gep_all_18z')
   
   for(m in 1:length(model_list)){
     
@@ -42,8 +36,8 @@ for(i in 1:length(urls.out$url)){
     
     #Check if already downloaded
     if(!file_present_local){
-      model.runs <- GetDODSModelRuns(model.url)
       
+      model.runs <- GetDODSModelRuns(model.url)
       #check if avialable at NOAA
       if(model_list[m] %in% model.runs$model.run){
         
@@ -69,49 +63,49 @@ for(i in 1:length(urls.out$url)){
         #pressfc #Surface pressure [pa]
         
         tmp2m <- DODSGrab(model.url, model.run, "tmp2m", time = time, lon = c(lon,lon), 
-                          
                           lat = c(lat,lat),ensembles=c(0,20))
         
         dlwrfsfc <- DODSGrab(model.url, model.run, "dlwrfsfc", time = time, lon = c(lon,lon), 
-                             
                              lat = c(lat,lat),ensembles=c(0,20))
         
         dswrfsfc <- DODSGrab(model.url, model.run, "dswrfsfc", time = time, lon = c(lon,lon), 
-                             
                              lat = c(lat,lat),ensembles=c(0,20))
         
         pratesfc <- DODSGrab(model.url, model.run, "pratesfc", time = time, lon = c(lon,lon), 
-                             
                              lat = c(lat,lat),ensembles=c(0,20))
         
         rh2m <- DODSGrab(model.url, model.run, "rh2m", time = time, lon = c(lon,lon), 
-                         
                          lat = c(lat,lat),ensembles=c(0,20))
         
         vgrd10m <- DODSGrab(model.url, model.run, "vgrd10m", time = time, lon = c(lon,lon), 
-                            
                             lat = c(lat,lat),ensembles=c(0,20))
         
         ugrd10m <- DODSGrab(model.url, model.run, "ugrd10m", time = time, lon = c(lon,lon), 
-                            
                             lat = c(lat,lat),ensembles=c(0,20))
         
         spfh2m <- DODSGrab(model.url, model.run, "spfh2m", time = time, lon = c(lon,lon), 
-                           
                            lat = c(lat,lat),ensembles=c(0,20))
         
         pressfc <- DODSGrab(model.url, model.run, "pressfc", time = time, lon = c(lon,lon), 
-                            
                             lat = c(lat,lat),ensembles=c(0,20))
         
-        forecast_noaa = data.frame(forecast.date = tmp2m$forecast.date,ensembles = tmp2m$ensembles,tmp2m = tmp2m$value, dlwrfsfc= dlwrfsfc$value, dswrfsfc = dswrfsfc$value, pratesfc = pratesfc$value,
-                                   
-                                   rh2m = rh2m$value, vgrd10m = vgrd10m$value, ugrd10m = ugrd10m$value, spfh2m = spfh2m$value, pressfc = pressfc$value)
+        forecast.time <- strftime(tmp2m$forecast.date, format="%Y-%m-%d %H:%M:%S",tz = 'GMT')
+        
+        forecast_noaa <- data.frame(forecast.date = forecast.time, 
+                                    ensembles = tmp2m$ensembles, 
+                                    tmp2m = tmp2m$value, 
+                                    dlwrfsfc= dlwrfsfc$value, 
+                                    dswrfsfc = dswrfsfc$value, 
+                                    pratesfc = pratesfc$value, 
+                                    rh2m = rh2m$value, 
+                                    vgrd10m = vgrd10m$value, 
+                                    ugrd10m = ugrd10m$value, 
+                                    spfh2m = spfh2m$value, 
+                                    pressfc = pressfc$value)
         
         write.csv(forecast_noaa,paste(directory,run_date,model_list[m],'.csv',sep=''),row.names = FALSE)
       }
     }
   }
-  
 }
 
